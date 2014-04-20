2a536823831e1b2021cc5e5e527d84272917ef8d
Gehad elrobey <gehadelrobey@gmail.com>
2014-04-20 11:18:58 +0200

    Adding Makefiles
    Signed-off-by: Gehad elrobey <gehadelrobey@gmail.com>

--- main.c
+++ main.c
@@ -13,8 +13,10 @@ int main(int argc,char* argv[]){
 	}
 	FILE *fp = fopen(argv[1],"r");
 	tokenize(fp);
+	printf("\e[1;31m");
 	printf("Name\tType\r\n");
 	printf("---------------\r\n");
+	printf("\e[0m");
 	while(head!=NULL){
 		printf("%s\t%s\r\n",head->value,type_name(head->type));
 		head=head->next;
